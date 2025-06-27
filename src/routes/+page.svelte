aef32e43b3db0e0dcecf5bf91d26e6eae14c741c
carviis <carlosvinicius.gomes.dc@gmail.com>
2025-06-27 11:12:29 -0300

    Add footer and style to app.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,3 @@
     <p>Made with ❤️ by SvelteKit</p>
     <p>Check out the <a href="https://svelte.dev/docs">SvelteKit documentation</a> for more information.</p>
 </footer>
-<style>
-    footer {
-        text-align: center;
-        margin-top: 20px;
-        font-size: 0.9em;
-        color: #666;
-    }
-    h1 {
-        color: purple;
-    }
-</style>
