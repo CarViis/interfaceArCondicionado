92764dcbe7b610b78cbadd089c364b13b04387db
carviis <carlosvinicius.gomes.dc@gmail.com>
2025-06-27 11:16:19 -0300

    Changed footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome to SvddelteKKKKKKKKKKKKKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <footer>
-    <p>Made with ❤️ by SvelteKit</p>
-    <p>Check out the <a href="https://svelte.dev/docs">SvelteKit documentation</a> for more information.</p>
+    <p>Feito por INFO 4M</p>
+    <p>Por Carlos e Erick</p>
 </footer>
