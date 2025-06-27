8d3d6226355ee3475931762d182a33187728ce9c
Erick o Romantico <erickrenner021@gmail.com>
2025-06-27 11:10:40 -0300

    create footer base

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <h1>Welcome to SvddelteKKKKKKKKKKKKKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
+<footer>
+    <p>Made with ❤️ by SvelteKit</p>
+    <p>Check out the <a href="https://svelte.dev/docs">SvelteKit documentation</a> for more information.</p>
+</footer>
 <style>
+    footer {
+        text-align: center;
+        margin-top: 20px;
+        font-size: 0.9em;
+        color: #666;
+    }
     h1 {
         color: purple;
     }
