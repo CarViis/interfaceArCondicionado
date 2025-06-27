6efe21e833701ed4e0e9c0e04070b6e92897cc59
carviis <carlosvinicius.gomes.dc@gmail.com>
2025-06-27 10:42:48 -0300

    First commit in rodape branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
+<p>This is a basic SvelteKit application.</p>
+<p>To get started, edit <code>src/routes/+page.svelte</code> and save to reload.</p>
+<p>For more information, visit the <a href="https://kit.svelte.dev">
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
