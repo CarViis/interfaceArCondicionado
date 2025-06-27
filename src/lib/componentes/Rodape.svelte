c176a40ad75d6535c42520608ac28b3999f488df
carviis <carlosvinicius.gomes.dc@gmail.com>
2025-06-27 11:52:26 -0300

    Copied footer

--- src/lib/componentes/Rodape.svelte
+++ src/lib/componentes/Rodape.svelte
@@ -1,8 +1,66 @@
 <script>
-    export let name = "Mundo";
+	export let name = 'Mundo';
 </script>
+
 <h1>teste</h1>
-<footer>
-    <p>Feito por INFO 4M</p>
-    <p>Por Carlos e Erick</p>
+<footer class="MuiBox-root css-dgy9ek">
+	<div class="MuiContainer-root MuiContainer-maxWidthLg css-1qsxih2">
+		<div class="MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-3 css-1h77wgb">
+			<div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 css-h3o6is">
+				<h6 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-1q8ouaq">
+					Sobre Nós
+				</h6>
+				<p class="MuiTypography-root MuiTypography-body2 MuiTypography-alignCenter css-rdifmp">
+					Copyright © <a
+						class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1i1yl23"
+						href="https://geatic.ifrn.edu.br">GEATIC</a
+					> 2025.
+				</p>
+				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">Email: geatic@ifrn.edu.br</p>
+			</div>
+			<div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 css-h3o6is">
+				<h6 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-1q8ouaq">
+					Contacte-nos
+				</h6>
+				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">
+					Estr. Guanduba, s/n, São Gonçalo do Amarante - RN, 59291-727
+				</p>
+				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">Telefone: (84) 4005-4111</p>
+			</div>
+			<div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 css-h3o6is">
+				<h6 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-1q8ouaq">
+					Redes Sociais
+				</h6>
+				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">
+					<a
+						class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1cfsjm"
+						href="https://www.instagram.com/geatic.ifrn/"
+						><svg
+							class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+							focusable="false"
+							aria-hidden="true"
+							viewBox="0 0 24 24"
+							data-testid="InstagramIcon"
+							><path
+								d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"
+							></path></svg
+						></a
+					>
+				</p>
+			</div>
+		</div>
+	</div>
 </footer>
+
+<style>
+	footer {
+		text-align: center;
+		margin-top: 20px;
+		font-size: 0.9em;
+		color: white;
+		background-color: black;
+	}
+	h1 {
+		color: purple;
+	}
+</style>
