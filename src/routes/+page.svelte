9501654873e1f709d51c5c92ce3731c0b38b426f
CarViis <carlosvinicius.gomes.dc@gmail.com>
2025-07-10 17:49:32 -0300

    Add Svelte Materials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script>
-	import Cabecalho from "$lib/componentes/Cabecalho.svelte";
-
+	import Cabecalho from '$lib/componentes/Cabecalho.svelte';
+	import Button from '@smui/button';
+	import Tab, { Label } from '@smui/tab';
+	import TabBar from '@smui/tab-bar';
+	import Fab, { Icon } from '@smui/fab';
+	import { mdiPlus } from '@mdi/js';
+	function cliquei() {
+		alert('Você clicou no botão!');
+	}
 </script>
-<Cabecalho>igjroigjrojgtr</Cabecalho>
+
+<Cabecalho />
+<Button onclick={cliquei} variant="raised" color="primary">Teste</Button>
+<Button onclick={cliquei} variant="raised" color="primary">Teste</Button>
+<div>
+  <TabBar tabs={[...Array(5)].map((_v, i) => i + 1)}>
+    {#snippet tab(tab)}
+      <Tab {tab}>
+        <Label>Tab {tab}</Label>
+      </Tab>
+    {/snippet}
+  </TabBar>
+</div>
