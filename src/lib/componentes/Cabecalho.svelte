998e9d7c174a71039e964e81f939ff3920b5dc51
Alisson Ribeiro <alissongbs91@gmail.com>
2025-07-11 11:20:42 -0300

    criação componente TabBar

--- src/lib/componentes/Cabecalho.svelte
+++ src/lib/componentes/Cabecalho.svelte
@@ -1,22 +1,12 @@
 <script>
     import Button from '@smui/button';
-	import Tab, { Label } from '@smui/tab';
-	import TabBar from '@smui/tab-bar';
-	import { mdiPlus } from '@mdi/js';
-	function cliquei() {
+	import TabBar from './TabBar.svelte';
+    function cliquei() {
 		alert('Você clicou no botão!');
 	}
 </script>
 
-<div>
-  <TabBar tabs={[...Array(5)].map((_v, i) => i + 1)}>
-    {#snippet tab(tab)}
-      <Tab {tab}>
-        <Label>Tab {tab}</Label>
-      </Tab>
-    {/snippet}
-  </TabBar>
-</div>
+<TabBar/>
 <Button onclick={cliquei} variant="raised" color="primary">Teste</Button>
 <Button onclick={cliquei} variant="raised" color="primary">Teste</Button>
 <style>
