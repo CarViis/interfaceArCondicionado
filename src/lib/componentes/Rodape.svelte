2c507aa5cd739d03ab8f1f2b43c71a20c8d6ed37
Erick o Romantico <erickrenner021@gmail.com>
2025-07-18 10:55:11 -0300

    Finalizado Rodape

--- src/lib/componentes/Rodape.svelte
+++ src/lib/componentes/Rodape.svelte
@@ -1,42 +1,36 @@
-<script>
-	export let name = 'Mundo';
-</script>
 
-<h1>teste</h1>
-<footer class="MuiBox-root css-dgy9ek">
-	<div class="MuiContainer-root MuiContainer-maxWidthLg css-1qsxih2">
-		<div class="MuiGrid-root MuiGrid-container MuiGrid-spacing-xs-3 css-1h77wgb">
-			<div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 css-h3o6is">
-				<h6 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-1q8ouaq">
+<footer>
+<div class="Conteiner-Rodape">
+	<div class="Conteiner-Rodape-2">
+			<div class="Conteiner-SobreNos">
+				<h6>
 					Sobre Nós
 				</h6>
-				<p class="MuiTypography-root MuiTypography-body2 MuiTypography-alignCenter css-rdifmp">
+				<p>
 					Copyright © <a
-						class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1i1yl23"
 						href="https://geatic.ifrn.edu.br">GEATIC</a
 					> 2025.
 				</p>
-				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">Email: geatic@ifrn.edu.br</p>
+				<p>Email: geatic@ifrn.edu.br</p>
 			</div>
-			<div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 css-h3o6is">
-				<h6 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-1q8ouaq">
+			<div class="Conteiner-Contato">
+				<h6 >
 					Contacte-nos
 				</h6>
-				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">
+				<p >
 					Estr. Guanduba, s/n, São Gonçalo do Amarante - RN, 59291-727
 				</p>
-				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">Telefone: (84) 4005-4111</p>
+				<p >Telefone: (84) 4005-4111</p>
 			</div>
-			<div class="MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-4 css-h3o6is">
-				<h6 class="MuiTypography-root MuiTypography-h6 MuiTypography-gutterBottom css-1q8ouaq">
+			<div class="Conteiner-RedesSociais">
+				<h6>
 					Redes Sociais
 				</h6>
-				<p class="MuiTypography-root MuiTypography-body2 css-u30lmt">
+				<p>
 					<a
-						class="MuiTypography-root MuiTypography-inherit MuiLink-root MuiLink-underlineAlways css-1cfsjm"
 						href="https://www.instagram.com/geatic.ifrn/"
 						><svg
-							class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
+							class="Conteiner-Img-Instagram"
 							focusable="false"
 							aria-hidden="true"
 							viewBox="0 0 24 24"
@@ -45,7 +39,7 @@
 								d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"
 							></path></svg
 						></a
-					>
+>
 				</p>
 			</div>
 		</div>
@@ -58,9 +52,42 @@
 		margin-top: 20px;
 		font-size: 0.9em;
 		color: white;
-		background-color: black;
+		background-color: gray;
+		display: flex;
+		position: absolute;
+		bottom:auto;
+		width: 100%;
+		flex-direction: row;
+		justify-content: center;
+        align-items: center;
+        padding: 32px;
 	}
-	h1 {
-		color: purple;
+	.Conteiner-Rodape {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
 	}
+	.Conteiner-Rodape-2 {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: flex-start;
+		width: 100%;
+		gap: 40px;
+	}
+    .Conteiner-RedesSociais {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	svg {
+		width: 24px;
+		height: 24px;
+	}
+    h6 {
+        font-size: x-large;
+    }
 </style>
