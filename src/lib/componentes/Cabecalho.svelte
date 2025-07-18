a216befbd62d14003cd881efe3d857302022b563
Carlos V. Gomes <151888549+CarViis@users.noreply.github.com>
2025-07-18 11:32:27 -0300

    Develop (#6)
    * teste
    
    * First commit in rodape branch
    
    * Text commit
    
    * teste2
    
    * teste3
    
    * Commit test
    
    * H1 color changed
    
    * Second commit message
    
    * create footer base
    
    * Add footer and style to app.css
    
    * Changed footer
    
    * Changed color background
    
    * Fix footer CSS formatting and ensure proper closure of rules
    
    * Commit1
    
    * Commit2
    
    * Add footer component and update main page to use it
    
    * Change footer bg color
    
    * mudei pra laranja
    
    * mudei pra v
    
    * Update footer content and style in app.css and Rodape.svelte
    
    * Copied footer
    
    * Add Svelte Materials
    
    * Remove fab from page
    
    * Coloquei a TabBar no Cabeçalho
    
    * criação componente TabBar
    
    * Colocar cabecalho no layout.svelte
    
    * Fix footer CSS formatting and improve color consistency
    
    * Adicionar LOGO à esquerda do TabBar
    
    * Finalizado Rodape
    
    * Adicionado Cabecalho e Rodape
    
    ---------
    
    Co-authored-by: Lucas L <lucaslourenco3264@gmail.com>
    Co-authored-by: Erick o Romantico <erickrenner021@gmail.com>
    Co-authored-by: Samuel Victor <victor.samuel@escolar.ifrn.edu.br>
    Co-authored-by: Alisson Ribeiro <alissongbs91@gmail.com>

---
+++ src/lib/componentes/Cabecalho.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Button from '@smui/button';
+	import TabBar from './TabBar.svelte';
+    function cliquei() {
+		alert('Você clicou no botão!');
+	}
+</script>
+
+<div class="cabecalho-container">
+    <img src="src/lib/Logo.png" alt="Logo" />
+    <TabBar/>
+</div>
+<!-- <Button onclick={cliquei} variant="raised" color="primary">Teste</Button>
+<Button onclick={cliquei} variant="raised" color="primary">Teste</Button> -->
+<style>
+    img {
+        height: 50%;
+    }
+    .cabecalho-container {
+        display: flex;
+        align-items: center;
+        gap: 24px;
+    }
+
+    h1{
+        color: orange
+    }
+</style>
---
+++ src/lib/componentes/Rodape.svelte
@@ -0,0 +1,93 @@
+
+<footer>
+<div class="Conteiner-Rodape">
+	<div class="Conteiner-Rodape-2">
+			<div class="Conteiner-SobreNos">
+				<h6>
+					Sobre Nós
+				</h6>
+				<p>
+					Copyright © <a
+						href="https://geatic.ifrn.edu.br">GEATIC</a
+					> 2025.
+				</p>
+				<p>Email: geatic@ifrn.edu.br</p>
+			</div>
+			<div class="Conteiner-Contato">
+				<h6 >
+					Contacte-nos
+				</h6>
+				<p >
+					Estr. Guanduba, s/n, São Gonçalo do Amarante - RN, 59291-727
+				</p>
+				<p >Telefone: (84) 4005-4111</p>
+			</div>
+			<div class="Conteiner-RedesSociais">
+				<h6>
+					Redes Sociais
+				</h6>
+				<p>
+					<a
+						href="https://www.instagram.com/geatic.ifrn/"
+						><svg
+							class="Conteiner-Img-Instagram"
+							focusable="false"
+							aria-hidden="true"
+							viewBox="0 0 24 24"
+							data-testid="InstagramIcon"
+							><path
+								d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5 5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3z"
+							></path></svg
+						></a
+>
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		margin-top: 20px;
+		font-size: 0.9em;
+		color: white;
+		background-color: gray;
+		display: flex;
+		position: absolute;
+		bottom:auto;
+		width: 100%;
+		flex-direction: row;
+		justify-content: center;
+        align-items: center;
+        padding: 32px;
+	}
+	.Conteiner-Rodape {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
+	.Conteiner-Rodape-2 {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: flex-start;
+		width: 100%;
+		gap: 40px;
+	}
+    .Conteiner-RedesSociais {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+	svg {
+		width: 24px;
+		height: 24px;
+	}
+    h6 {
+        font-size: x-large;
+    }
+</style>
