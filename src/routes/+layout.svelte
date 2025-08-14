ee453d45e727548c96ef9e76a37f7c6473316a2d
Carlos V. Gomes <151888549+CarViis@users.noreply.github.com>
2025-08-13 23:08:06 -0300

    Programação (#12)
    * Adicionado nova página programação e adicionado nova tabela de horários
    
    * Criado componente da Tabela de Horários na página de programação

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,24 @@
 	import Cabecalho from '$lib/componentes/Cabecalho.svelte';
 	import Rodape from '$lib/componentes/Rodape.svelte';
 	import BarraLateral from '$lib/componentes/BarraLateral.svelte';
+	import { page } from '$app/stores';
+	let { children } = $props();
 </script>
 
+
+
 <div style="min-height: 100vh; display: flex; flex-direction: column;">
+	{#if $page.url.pathname !== '/programacao'}
 	<Cabecalho />
+  {/if}
 	<div style="flex: 1; display: flex; min-height: 0;">
 		<BarraLateral />
+    {@render children()}
 		<div style="flex: 1; overflow: auto;">
 			<slot />
 		</div>
 	</div>
+	{#if $page.url.pathname !== '/programacao'}
 	<Rodape />
+  {/if}
 </div>
---
+++ src/routes/programacao/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import TabelaHorario from "$lib/componentes/TabelaHorario.svelte";
+</script>
+
+
+<div style="text-align: center;">
+    <h2>Campus YYYYYYY - Sala XXXXXX</h2>
+</div>
+<TabelaHorario/>
